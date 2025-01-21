db75f9237cdec813838fad18c5694b3d0ac3e4da
AlkisAzna <alkaznavouridis@gmail.com>
2025-01-21 20:15:22 +0200

    Initialize Repo

---
+++ retrieval-manager/retrieval-manager/src/main/java/gr/brainy/RetrievalManagerApplication.java
@@ -0,0 +1,13 @@
+package gr.brainy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrievalManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrievalManagerApplication.class, args);
+	}
+
+}
